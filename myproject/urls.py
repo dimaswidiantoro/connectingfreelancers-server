6bccbc2aade28da7622d5e9885c679de82d9e424
dimaswidiantoro <tjitrodimedjo@gmail.com>
2018-10-25 13:52:34 +0000

    finally it works

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from posts import views
@@ -23,3 +23,4 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^posts/', include('posts.urls')),
     url(r'^$', views.home, name="home"),
+]
